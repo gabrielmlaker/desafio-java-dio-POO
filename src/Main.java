389bcc712c0904a9f583546d1500304e436f58e0
Gabriel Amed Mlaker da Silva <mlakergabriel@gmail.com>
2022-08-15 01:29:58 -0300

    create first class commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+
+        curso2.setTitulo("Curso JS");
+        curso2.setDescricao("Descrição curso JS");
+        curso2.setCargaHoraria(6);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descricao mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        Mentoria mentoria2 = new Mentoria();
+        mentoria2.setTitulo("Mentoria de JS");
+        mentoria2.setDescricao("Descricao mentoria JS");
+        mentoria2.setData(LocalDate.now());
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+        System.out.println(mentoria2);
+    }
+}
